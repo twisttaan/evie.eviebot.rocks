28d6e469f01f0afe55900c8ee3fce3fc5871dc35
Tristan Camejo <twisttaan@icloud.com>
2021-11-29 21:31:39 +1100

    Initial Site

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,27 @@
 <script>
-	export let name;
+  export let evie;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <head>
+    <!-- Main Tags -->
+    <title>evie</title>
+    <meta name="title" content="evie" />
+    <meta name="description" content="" />
+    <meta name="theme-color" content="#77a168" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- Open Graph -->
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://evie.eviebot.rocks/" />
+    <meta property="og:title" content="evie" />
+    <meta property="og:description" content="" />
+    <meta property="og:image" content={evie} />
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta property="twitter:url" content="https://evie.eviebot.rocks/" />
+    <meta property="twitter:title" content="evie" />
+    <meta property="twitter:description" content="" />
+    <meta property="twitter:image" content={evie} />
+  </head>
+  <img src={evie} alt="amazing evie picture" />
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
-import App from './App.svelte';
+import App from "./App.svelte";
+
+// get a random picture from /evies
+const randomPicture = () => {
+  const random = Math.floor(Math.random() * 10);
+  return `/evies/${random + 1}.jpg`;
+};
 
 const app = new App({
-	target: document.body,
-	props: {
-		name: 'world'
-	}
+  target: document.body,
+  props: {
+    evie: randomPicture(),
+  },
 });
 
 export default app;
