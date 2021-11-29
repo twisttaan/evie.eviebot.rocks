0490ed009fb74c6874796a2d50b669271c2e8f3e
Tristan Camejo <twisttaan@icloud.com>
2021-11-29 21:35:12 +1100

    fix: tags

--- src/App.svelte
+++ src/App.svelte
@@ -8,20 +8,20 @@
     <title>evie</title>
     <meta name="title" content="evie" />
     <meta name="description" content="" />
-    <meta name="theme-color" content="#77a168" />
+    <meta name="theme-color" content="#FF10F0" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <!-- Open Graph -->
     <meta property="og:type" content="website" />
     <meta property="og:url" content="https://evie.eviebot.rocks/" />
     <meta property="og:title" content="evie" />
     <meta property="og:description" content="" />
-    <meta property="og:image" content={evie} />
+    <meta property="og:image" content="https://evie.eviebot.rocks{evie}" />
     <!-- Twitter -->
     <meta property="twitter:card" content="summary_large_image" />
     <meta property="twitter:url" content="https://evie.eviebot.rocks/" />
     <meta property="twitter:title" content="evie" />
     <meta property="twitter:description" content="" />
-    <meta property="twitter:image" content={evie} />
+    <meta property="twitter:image" content="https://evie.eviebot.rocks{evie}" />
   </head>
   <img src={evie} alt="amazing evie picture" />
 </main>
