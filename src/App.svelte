b8dd826ad782f71facea238c13eb3c99395d4bab
Tristan Camejo <twisttaan@icloud.com>
2021-11-29 21:40:52 +1100

    fix: head tag

--- src/App.svelte
+++ src/App.svelte
@@ -3,32 +3,6 @@
 </script>
 
 <main>
-  <head>
-    <!-- Main Tags -->
-    <title>evie</title>
-    <meta name="title" content="evie" />
-    <meta name="description" content="" />
-    <meta name="theme-color" content="#FF10F0" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <!-- Open Graph -->
-    <meta property="og:type" content="website" />
-    <meta property="og:url" content="https://evie.eviebot.rocks/" />
-    <meta property="og:title" content="evie" />
-    <meta property="og:description" content="" />
-    <meta
-      property="og:image"
-      content="https://evie.eviebot.rocks/evies/5.jpg"
-    />
-    <!-- Twitter -->
-    <meta property="twitter:card" content="summary_large_image" />
-    <meta property="twitter:url" content="https://evie.eviebot.rocks/" />
-    <meta property="twitter:title" content="evie" />
-    <meta property="twitter:description" content="" />
-    <meta
-      property="twitter:image"
-      content="https://evie.eviebot.rocks/evies/5.jpg"
-    />
-  </head>
   <!-- svelte-ignore a11y-missing-attribute -->
   <img src={evie} />
 </main>
